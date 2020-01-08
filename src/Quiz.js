16b400a9773ca874285616aa493d26bed19a9fc1
Michael Moody <dcmoods@gmail.com>
2020-01-08 10:47:30 -0500

    module 6 update

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,7 +10,12 @@ class Quiz extends Component {
         this.state = {
             quiz_position: 1
         };
+    }
 
+    showNextQuestion = () => {
+        this.setState((state) => {
+            return { quiz_position: state.quiz_position + 1};
+        })
     }
 
     render() {
@@ -19,7 +24,10 @@ class Quiz extends Component {
             <div>
                 {isQuizEnd ? 
                     <QuizEnd /> :
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+                    <QuizQuestion 
+                        quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+                        showNextQuestionHandler={this.showNextQuestion.bind(this)}
+                        />}
             </div>
             );
     }
