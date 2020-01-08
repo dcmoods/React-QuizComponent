c9ba04a91192bf6f4c92a9796fe961087151e5a4
Michael Moody <dcmoods@gmail.com>
2020-01-08 11:00:47 -0500

    update m7

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,9 +4,7 @@ import QuizQuestionButton from './QuizQuestionButton';
 class QuizQuestion extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            incorrectAnswer: false
-        };
+        this.state = { incorrectAnswer: false };
     }
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer)  {
